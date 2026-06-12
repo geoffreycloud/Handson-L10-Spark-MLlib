a8bbee0c417b8cfaa145bf7e2f44004d4f664f2d
Geoffrey <gcloud@uncc.edu>
2026-06-12 16:02:44 -0400

    task 4 complete with sample outputs

--- task4.py
+++ task4.py
@@ -5,6 +5,8 @@ from pyspark.sql.types import StructType, StructField, StringType, IntegerType, 
 
 # Import necessary MLlib classes
 # TODO: Import VectorAssembler, LinearRegression, and LinearRegressionModel
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.regression import LinearRegression, LinearRegressionModel
 
 # Create Spark Session
 spark = SparkSession.builder.appName("Task4_FarePrediction_Assignment").getOrCreate()
@@ -24,25 +26,29 @@ if not os.path.exists(MODEL_PATH):
 
     # TODO: Cast `distance_km` and `fare_amount` columns to DoubleType for ML
     # HINT: Use the .withColumn() and .cast() methods.
-    train_df = None # Replace None with your implementation
+    train_df = train_df_raw.withColumn("distance_km", col("distance_km").cast(DoubleType())) \
+                           .withColumn("fare_amount", col("fare_amount").cast(DoubleType()))
 
     # TODO: Create a VectorAssembler to combine feature columns into a single 'features' vector.
     # The input column should be 'distance_km'.
-    assembler = None # Replace None with your implementation
-    train_data_with_features = None # Replace None with your implementation
+    assembler = VectorAssembler(inputCols=["distance_km"], outputCol="features")
+    train_data_with_features = assembler.transform(train_df)
 
     # TODO: Create a LinearRegression model instance.
     # Set the features column to 'features' and the label column to 'fare_amount'.
-    lr = None # Replace None with your implementation
+    lr = LinearRegression(featuresCol="features", labelCol="fare_amount")
 
     # TODO: Train the model by fitting it to the training data.
-    model = None # Replace None with your implementation
+    model = lr.fit(train_data_with_features)
 
     # TODO: Save the trained model to the specified MODEL_PATH.
     # HINT: Use the .write().overwrite().save() methods.
+
     print(f"[Training Complete] Model saved to -> {MODEL_PATH}")
+    model.write().overwrite().save(MODEL_PATH)
 else:
     print(f"[Model Found] Using existing model from {MODEL_PATH}")
+    model = LinearRegressionModel.load(MODEL_PATH)
 
 
 # --- PART 2: STREAMING INFERENCE ---
@@ -67,26 +73,44 @@ raw_stream = spark.readStream.format("socket") \
 parsed_stream = raw_stream.select(from_json(col("value"), schema).alias("data")).select("data.*")
 
 # TODO: Load the pre-trained LinearRegressionModel from MODEL_PATH.
-model = None # Replace None with your implementation
+model = LinearRegressionModel.load(MODEL_PATH)
 
 # TODO: Use a VectorAssembler to transform the `distance_km` column of the streaming data
 # into a 'features' vector. This must be the same transformation as in the training phase.
-assembler_inference = None # Replace None with your implementation
-stream_with_features = None # Replace None with your implementation
+
+assembler_inference = VectorAssembler(inputCols=["distance_km"], outputCol="features")
+stream_with_features = assembler_inference.transform(parsed_stream)
 
 # TODO: Use the loaded model to make predictions on the streaming data.
 # HINT: Use model.transform()
-predictions = None # Replace None with your implementation
+predictions = model.transform(stream_with_features) 
 
 # TODO: Calculate the 'deviation' between the actual 'fare_amount' and the 'prediction'.
 # HINT: Use withColumn and the abs_diff function.
-predictions_with_deviation = None # Replace None with your implementation
+predictions_with_deviation = predictions.withColumn("deviation", abs_diff(col("fare_amount") - col("prediction"))) 
 
 # Select the final columns to display in the output
 output_df = predictions_with_deviation.select(
     "trip_id", "driver_id", "distance_km", "fare_amount",
     col("prediction").alias("predicted_fare"), "deviation"
 )
+
+def write_batch(batch_df, batch_id):
+
+    print(f"\n===== Batch {batch_id} =====")
+    batch_df.show(truncate=False)
+
+    # Save the batch DataFrame as a CSV file with the batch ID in the filename
+    batch_df.coalesce(1).write \
+        .mode("append") \
+        .option("header", True) \
+        .csv("outputs/task_4")
+
+query = output_df.writeStream \
+    .outputMode("append") \
+    .foreachBatch(write_batch) \
+    .trigger(processingTime="10 seconds") \
+    .start()
 
 # Write the final results to the console
 query = output_df.writeStream \
